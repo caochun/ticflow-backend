854fe552d3a24f7352d628b79c1ed87b8cb0e5ca
lzl <nju10lzl@gmail.com>
2016-06-05 16:41:40 +0800

    modify profit

--- routes/app/profits.js
+++ routes/app/profits.js
@@ -4,6 +4,7 @@ var eventproxy = require('eventproxy');
 
 var mongoose = require('mongoose');
 var Profit = require('../../models/Profit.js');
+var Factor = require('../../models/Factor.js');
 
 router.get('/expense', function (req, res) {
 	var page = (req.query.page === undefined) ? 0 : req.query.page;
@@ -39,38 +40,46 @@ router.get('/totalexpense', function (req, res) {
 	});
 });
 
-router.get('/profit', function (req, res) {
-	var page = (req.query.page === undefined) ? 0 : req.query.page;
-	var limit = (req.query.limit === undefined) ? 10 : parseInt(req.query.limit);
-
-	delete req.query.page;
-	delete req.query.limit;
-
-	req.query.detail = 'profit';
-
-	Profit.find(req.query).select('serial_number money comment').skip(page * limit).limit(limit).sort({create_at: -1}).exec(function (err, profit) {
-		if (err) {
-			return res.status(400).send("err in get /profits/profit");
-		} else {
-			return res.status(200).json(profit);
-		}
-	});
-});
-
 router.get('/totalprofit', function (req, res) {
-	req.query.detail = 'profit';
-
-	Profit.find(req.query, function (err, profit) {
-		if (err) {
-			return res.status(400).send("err in get /profits/profit");
-		} else {
-			var totalprofit = 0;
-			profit.forEach(function (entry) {
-				totalprofit += entry.money;
-			});
-			return res.status(200).json(totalprofit);
-		}
-	});
+	var factor;
+	var ep = new eventproxy();
+	Factor.findOne({id: "default"}, function (err, data) {
+		factor = data.value;
+    	ep.emit('factor');
+  	});
+  	var totalprofit = 0;
+  	ep.on('factor', function () {
+  		Profit.find(req.query).distinct('month', function (err, months) {
+  			if (months.length == 0)
+  				return res.status(200).json(totalprofit);
+			for (var i = 0; i < months.length; i++) {
+	      		ep.on('month' + i, (function (i) {
+	        		return function () {
+	        			req.query.month = months[i];
+	        			var curprofit = 0;
+	          			Profit.find(req.query).exec(function (err, profit) {
+	            			profit.forEach(function (entry) {
+	            				if (entry.detail == "profit")
+									curprofit += entry.money;
+								else
+									curprofit -= entry.money;
+	            			});
+	            			if (curprofit < 0)
+	            				totalprofit += curprofit;
+	            			else
+	            				totalprofit += curprofit * factor;
+	            			ep.emit('month' + (i + 1));
+	          			});
+	        		};
+	      		})(i));
+	    	}
+	    	ep.emit('month' + 0);
+	    	ep.on('month' + months.length, function () {
+		  		return res.status(200).json(totalprofit);
+		  	});
+	    });
+  	});
+  	
 });
 
 router.get('/months', function (req, res) {
